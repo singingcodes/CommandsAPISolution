8184ff1603b0087bf2a0baeed7feedaa4ed6680a
Sarah Ayanwale <sarahayanwale@gmail.com>
2021-06-14 17:28:13 +0100

    added model and data

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using CommandAPI.Data;
+using CommandAPILibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandAPI.Controllers
@@ -7,10 +9,31 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly ICommandAPIRepo _repository;
+        public CommandsController(ICommandAPIRepo repository)
         {
-            return new string[] {"this", "is", "hard", "coded"};
+            _repository = repository;
+        }
+        // [HttpGet]
+        // public ActionResult<IEnumerable<string>> Get()
+        // {
+        //     return new string[] {"this", "is", "hard", "coded"};
+        // }
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands ()
+        {
+            var CommandItems = _repository.GetAllCommands();
+            return Ok(CommandItems);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById (int id)
+        {
+            var CommandItem = _repository.GetCommandById(id);
+            if (CommandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(CommandItem);
         }
     }
 }
